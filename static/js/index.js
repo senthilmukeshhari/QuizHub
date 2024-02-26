27d782a77af3d66b5016663aba4841fe85da5aee
senthilmukeshhari <154602811+senthilmukeshhari@users.noreply.github.com>
2024-02-26 19:33:47 +0530

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,9 @@
+window.history.forward()
+
 window.addEventListener('load', () => {
     setTimeout(() => {
         let spinner = document.querySelector('.spinner')
 
         spinner.classList.add('d-none')
-    }, 2800);
+    }, 2700);
 })
